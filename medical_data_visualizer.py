46645dad96bbd2d965d163bf7cc505ad61e1a34f
Pietra Batista <pietra.batista@sou.inteli.edu.br>
2024-09-23 16:33:10 +0000

    versao 1 do projeto 3

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,31 +4,31 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv('medical_examination.csv')
 
 # 2
-df['overweight'] = None
+df['overweight'] = np.where(df['weight'] / (df['height'] ** 2) > 25, 1, 0)
 
 # 3
-
+df['cholesterol'] = df['cholesterol'].apply(lambda x: 0 if x == 1 else (1 if x > 1 else x))
+df['gluc'] = df['gluc'].apply(lambda x: 0 if x == 1 else (1 if x > 1 else x))
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
-
+    df_cat = pd.melt(df, 
+                     id_vars=['cardio'],  # Mantemos a coluna cardio
+                     value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
 
     # 6
-    df_cat = None
+    df_cat = df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index(name='counts')
     
-
     # 7
 
-
-
+    cat_plot = sns.catplot(x='variable', hue='value', col='cardio', data=df_cat, kind='count')
+    
     # 8
-    fig = None
-
+    fig = plot.fig
 
     # 9
     fig.savefig('catplot.png')
